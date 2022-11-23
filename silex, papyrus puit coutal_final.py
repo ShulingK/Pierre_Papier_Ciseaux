69a3d331b807804decfefd9b91d16152041ac23e
ShulingK <timotheimbert3@gmail.com>
2022-11-23 11:45:10 +0100

    error fixed

--- silex, papyrus puit coutal_final.py
+++ silex, papyrus puit coutal_final.py
@@ -13,25 +13,31 @@ def round(eventJoueurUn, eventJoueurDeux):
 #   si joueurUnAction est inferieur a 0 ou supérieur à 2 
     if(joueurUnAction < 0 or joueurDeuxAction > 2):
 #       alors retourner le message "demande incorrect"
+        print("demande incorrecte\n")
         return "demande incorrecte"
 #   si joueurUnAction est égal à joueurDeuxAction 
     if(joueurUnAction == joueurDeuxAction):
 #       alors retourner le message "match nul"
+        print("match nul\n")
         return "match nul"
 #   si joueurUnAction est égale à
     if(joueurUnAction == 9):
 #       alors retourner le message "manche gagnée par joueur 1"
+        print("manche gagnée par joueur 1\n")
         return "manche gagnée par joueur 1"
 #   si joueurDeuxAction est égale à 9 
     if(joueurDeuxAction == 9):
 #       alors retourner le message "manche gagnée par joueur 2"
+        print("manche gagnée par joueur 2\n")
         return "manche gagnée par joueur 2"
 #   si joueurUnAction est supérieur à joueurDeuxAction et que joueurDeuxAction plus 1 est égal à joueurUnAction
 #   ou que joueurUnAction est inférieur à joueurDeuxAction et que la somme de joueurUnAction et joueurDeuxAction donne 3 
     if(joueurUnAction > joueurDeuxAction and joueurDeuxAction + 1 == joueurUnAction or joueurUnAction < joueurDeuxAction and joueurUnAction + joueurDeuxAction == 3 ):   
 #       alors retourner le message "manche gagnée par joueur 1"
+        print("manche gagnée par joueur 1\n")
         return "manche gagnée par joueur 1"
 #   sinon retourner le message "manche gagnée par joueur 2"
+    print("manche gagnée par joueur 2\n")
     return "manche gagnée par joueur 2"
 
 ##jeu en 1vs1
@@ -49,20 +55,20 @@ def game():
     #   répéter indéfiniment dans une boucle tant que  
         while True:
     #       assigner à inputClavierUn l'information de l'input du clavier sous format int sachant que ( silex (0) papyrus (1) coutal (2) afficher score (4) fin (5))
-            inputClavierUn = int(input("commencer à jouer(3) afficher score (4) fin (5) BO(6)"))
+            inputClavierUn = int(input("commencer à jouer(3) afficher score (4) fin (5) BO(6) ?: "))
     #       si inputClavierUn est égal à 4
             if(inputClavierUn == 4):
     #           alors afficher joueurScore et botScore 
-                print(joueurScore, botScore)
+                print(joueurScore,"à", botScore, "\n")
     #       sinon si inputClavierUn est égal à 5
             elif(inputClavierUn == 5):
     #           alors si joueurScore est supérieur à botScore
                 if(joueurScore > botScore):
     #               alors afficher "bravo à vous!!!"
-                    print("bravo à vous!!!")
+                    print("bravo à vous!!!\n\n\n")
     #           sinon afficher "dommage..."
                 else: 
-                    print("quel dommage...")
+                    print("quel dommage...\n\n\n")
     #           sortir de la boucle 
                 break
     #       sinon si inputClavierUn est égal à 3
@@ -88,9 +94,9 @@ def game():
     #       sinon inputClavierUn est éga l à 6
             elif(inputClavierUn == 6):
     #           assigner à nbrTours l'information de l'input du clavier sous format int "le nombre de manche necessaire pour gagner"
-                nbrTours = int(input("le nombre de manche necessaire pour gagner"))
+                nbrTours = int(input("le nombre de manche necessaire pour gagner ?: "))
     #           assigner à joueurInput l'information de l'input du clavier sous format int ( silex (0) papyrus (1) coutal (2) ?)
-                joueurInput = int(input("silex (0) papyrus (1) coutal (2) ?"))
+                joueurInput = int(input("silex (0) papyrus (1) coutal (2) ?: "))
     #           assigner à botGame un nombre random sous format int entre 0 et 2
                 botGame = random.randint(0, 2)
     #            si result est égal à "manche gagnée par joueur 1 "
@@ -125,11 +131,11 @@ def game():
     #   répéter indéfiniment dans une boucle tant que   
         while True:
     #       assigner à inputClavierUn l'information de l'input du clavier sous format int sachant que ( silex (0) papyrus (1) coutal (2) afficher score (4) fin (5))
-            inputClavierUn = int(input("commencer à jouer(3) afficher score (4) fin (5) BO(6)"))
+            inputClavierUn = int(input("commencer à jouer(3) afficher score (4) fin (5) BO(6) ?: "))
     #       si inputClavierUn est égal à 4
             if(inputClavierUn == 4):
     #           alors afficher joueurUnScore et joueurDeuxScore
-                print(joueurUnScore, joueurDeuxScore)
+                print(joueurUnScore," à ", joueurDeuxScore, "\n")
     #       sinon si inputClavierUn est égal à 5
             elif(inputClavierUn == 5):
     #           alors si joueurUnScore est supérieur à joueurDeuxScore
@@ -144,9 +150,9 @@ def game():
     #       sinon si inputClavierUn est égal à 3
             elif(inputClavierUn == 3):
     #           assigner à joueurUnInput l'information de l'input du clavier sous format int ( silex (0) papyrus (1) coutal (2) ?)
-                joueurUnInput = int(input("silex (0) papyrus (1) coutal (2) ?"))
+                joueurUnInput = int(input("silex (0) papyrus (1) coutal (2) ?: "))
     #           assigner à joueurDeuxInput l'information de l'input du clavier sous format int ( silex (0) papyrus (1) coutal (2) ?)
-                joueurDeuxInput = int(input("silex (0) papyrus (1) coutal (2) ?"))
+                joueurDeuxInput = int(input("silex (0) papyrus (1) coutal (2) ?: "))
     #           assigner à result le retour de l'execultion de la fonction round avec les parametres joueurUnInput et joueurDeuxInput
                 result = round  (joueurUnInput, joueurDeuxInput)
     #           si result est égal à "manche gagnée par joueur 1"
@@ -163,11 +169,11 @@ def game():
     #       sinon inputClavierUn est égal à 6
             elif(inputClavierUn == 6):
     #           assigner à nbrTours l'information de l'input du clavier sous format int "le nombre de manche necessaire pour gagner"
-                nbrTours = int(input("le nombre de manche necessaire pour gagner ?"))
+                nbrTours = int(input("le nombre de manche necessaire pour gagner ?: "))
     #           assigner à joueurUnInput l'information de l'input du clavier sous format int ( silex (0) papyrus (1) coutal (2) ?)
-                joueurUnInput = int(input("silex (0) papyrus (1) coutal (2) ?"))
+                joueurUnInput = int(input("silex (0) papyrus (1) coutal (2) ?: "))
     #           assigner à joueurDeuxInput l'information de l'input du clavier sous format int ( silex (0) papyrus (1) coutal (2) ?)
-                joueurDeuxInput = int(input("silex (0) papyrus (1) coutal (2) ?"))
+                joueurDeuxInput = int(input("silex (0) papyrus (1) coutal (2) ?: "))
     #           assigner à result le retour de l'execultion de la fonction round avec les parametres joueurUnInput et joueurDeuxInput
                 result = round(joueurUnInput, joueurDeuxInput)
                 if(result == "manche gagnée par joueur1"):
